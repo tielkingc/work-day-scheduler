83dc5273cc6dcb2777f7c317f0a0b0842974dd2c
Christian Tielking <tielking5@gmail.com>
2021-09-25 16:13:37 -0400

    redid site logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,58 @@
-var tasks = ["","","","","","","","",""]
-var taskELs = document.getElementsByClassName("task");
-// task text was clicked
-for (var c = 0; c < 9; c++) {
-    $(".task" + c).on("click", "p", function() {
-        // get current text of p element
-        var text = $(this)
-        .text()
-        .trim();
-        
-        var currentClass = document.getElementsByClassName("task" +c).className
-        console.log(currentClass)
-        // replace p element with a new textarea
-        var textInput = $("<textarea>").addClass(currentClass).val(text);
-        
-        $(this).replaceWith(textInput);
-    
-        // auto focus new element
-        textInput.trigger("focus");
-    });
-    
-    // editable field was un-focused
-    $(".task" + c).on("blur", "textarea", function() {
-        // get current value of textarea
-        var text = $(this).val();
-    
-        // get status type and position in the list
-        var status = $(this)
-        .closest(".task")
-        .attr("id");
-        var index = $(this)
-        .closest(".task")
-        .index();
-    
-        // update task in array and re-save to localstorage
-        //tasks[status][index].text = text;
-        //saveTasks();
-    
-        // recreate p element
-        var taskP = $("<p>")
-        .text(text);
-    
-        // replace textarea with new content
-        $(this).replaceWith(taskP);
-    });
-}
-  var auditTask = function() {
-    // convert to moment object at 5:00pm
-    var realTime = moment().hour();
-    
-    var spanEls = document.getElementsByClassName("site-time");
-    
-    for ( var b = 0; b<9; b++){
-        
-        
-        var time = $(spanEls[b]).text().trim();
-        time = parseInt(time) 
-            
-        // remove any old classes from element
-        
-        if (time < 9) {
-            time = time + 12
-        }
-        // apply new class if task is near/over due date
-        if (time < realTime) {
-            $(".task" + b).removeClass("description past future present");
-            $(".task" + b).addClass("past");
-            //console.log("past")
-        }
-        else if (time > realTime) {
-            $(".task" + b).removeClass("description past future present");
-            
-            $(".task" + b).addClass("future");
-            //console.log("future")
-        }
-        else if (moment().hour() === time){
-            
-            $(".task" + b).removeClass("description");
-            $(".task" + b).addClass("present")
-        }
-        
-    }
-    
-  };
+$(document).ready(function(){
+    //Click listener for the save buttons
+    $(".saveBtn").on("click", function () {
+        console.log($(this).siblings(".description"));
+        var userInput = $(this).siblings(".description").val().trim();
+        console.log(userInput);
+        var timeSlot = $(this).prev().attr("time");
+        console.log(timeSlot);
 
+        //Saves to local storage
+        localStorage.setItem(timeSlot, JSON.stringify(userInput));
 
-for (var saNum = 0; saNum < 9; saNum++) {
-    $(".but" + saNum).on("click", function () {
-        var saveTask = $(".but" + saNum).closest(".task" + saNum).text();
-        tasks[saNum] = saveTask;
-        console.log(tasks);
-        $(".saveBtn").addClass(".saveBtn")
+        //localStorage.setItem(timeSlot, userInput)
     })
-}  
 
-setInterval(function() {
-    auditTask();
-}, 100);
+    //This will load any of the saved data that was stored in local storage and display it for the user on the page
+    $("#9").val(localStorage.getItem("9"));
+    $("#10").val(localStorage.getItem("10"));
+    $("#11").val(localStorage.getItem("11"));
+    $("#12").val(localStorage.getItem("12"));
+    $("#13").val(localStorage.getItem("13"));
+    $("#14").val(localStorage.getItem("14"));
+    $("#15").val(localStorage.getItem("15"));
+    $("#16").val(localStorage.getItem("16"));
+    $("#17").val(localStorage.getItem("17"));
+
+})
+
+
+
+$(document).ready(function(){
+    //Adds the data to the top of the work day scheduler
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    //Gets the current hour
+    var currenttime = moment().hours();
+
+    //Allows for highlightening 
+    $( "textarea" ).each(function() {
+        console.log(typeof $( this ).attr("time") );
+        console.log('time', typeof currenttime)
+
+        //This turns the current hour time-slot red
+        if (currenttime === parseInt($( this ).attr("time"))  ) {
+            $(this).addClass("present");
+        }
+
+        //This turns the time blocks coming up green
+        else if (currenttime > parseInt($( this ).attr("time"))  ) {
+            $(this).addClass("past");
+        }
+
+        //This turns the time blocks that have passes gray
+        else if (currenttime < parseInt($( this ).attr("time"))  ) {
+            $(this).addClass("future");
+        }
+      });
+
+})
